ffc088ecb632a539e6e66eff7c69f0fb6b7e969b
est31 <MTest31@outlook.com>
2017-12-24 04:15:10 +0100

    Add structopt for command line parsing

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,18 @@
-fn main() {}
+extern crate structopt;
+#[macro_use]
+extern crate structopt_derive;
+
+use structopt::StructOpt;
+
+#[derive(StructOpt, Debug)]
+#[structopt(about = "unused lint checker")]
+struct Options {
+	/// Override for the ulint toml
+	#[structopt(help = "Path to ulint.toml")]
+	toml: Option<String>,
+}
+
+fn main() {
+	let Options = Options::from_args();
+	// TODO
+}
